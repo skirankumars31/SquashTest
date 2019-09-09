b39383d4d4900ba3ce5128cf215efe936feed0bf
Kiran Kumar Sukumar <Kiran.Kumar.Sukumar@nav.no>
2019-09-09 11:32:55 +0200

    Fixed the bug in feature 1

--- src/main/java/no/midlertig/App.java
+++ src/main/java/no/midlertig/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("This is feature 1");
+        System.out.println("This is the implementation of feature 1");
     }
 }
