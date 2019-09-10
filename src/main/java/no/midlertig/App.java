0a9aff2df289de38e105d75f2128493057de619c
Kiran Kumar Sukumar <skirankumars31@gmail.com>
2019-09-10 12:09:10 +0200

    Implementation of feature 7

--- src/main/java/no/midlertig/App.java
+++ src/main/java/no/midlertig/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println("This is the implementation of feature 4");
         System.out.println("This is the implementation of feature 5");
         System.out.println("This is the implementation of feature 6");
+        System.out.println("This is the implementation of feature 7");
     }
 }
